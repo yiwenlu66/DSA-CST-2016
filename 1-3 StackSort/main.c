4b85c7142660edfc48a5f235f2927e076ce8303a
Peter Lu <mcfloundinho@gmail.com>
2016-10-20 09:16:06 +0800

    1-3: done

---
+++ 1-3 StackSort/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define MAXN 2000000
+#define BUF_SZ 1 << 22
+
+int seen[MAXN]; // boolean values, whether the number has been read
+int stack[MAXN];
+int top = 0;
+
+char ibuf[BUF_SZ], obuf[BUF_SZ];
+
+int main()
+{
+    setvbuf(stdin, ibuf, _IOFBF, BUF_SZ);
+    setvbuf(stdout, obuf, _IOFBF, BUF_SZ);
+    int n, max_unseen, curr;
+    scanf("%d", &n);
+    max_unseen = n;
+    while (n-- > 0) {
+        while (top > 0 && stack[top - 1] > max_unseen) {
+            printf("%d ", stack[--top]);
+        }
+        scanf("%d", &curr);
+        seen[curr] = 1;
+        if (curr == max_unseen) {
+            printf("%d ", curr);
+            while (seen[max_unseen]) {
+                --max_unseen;
+            }
+        } else {
+            stack[top++] = curr;
+        }
+    }
+    while (top > 0) {
+        printf("%d ", stack[--top]);
+    }
+    printf("\n");
+    return 0;
+}
