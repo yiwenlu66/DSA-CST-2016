99275cd147979b7503e4949cf6971791e1bc80ce
Peter Lu <mcfloundinho@gmail.com>
2016-12-26 01:59:37 +0800

    3-4: AC

--- 3-4 Company/main.c
+++ 3-4 Company/main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
-#define MAXN 10000001
+#include <stdint.h>
+#define BUF_SZ (1 << 20)
+#define MOD 519989
 
-int users[MAXN];        // map user rank to position in code stack, 0 for N/A
-int codestack[MAXN];    // stack for codes of online users, starts from 1, -1 for offline
-int userstack[MAXN];    // stack for user verification, starts from 1
-int top = 1;            // sentinel off the end of the stack
-int nonline = 0;        // number of online users
+char ibuf[BUF_SZ];
+
+int users[MOD];            // map user rank to position in code stack, 0 for N/A
+int codestack[MOD + 1];    // stack for codes of online users, starts from 1, -1 for offline
+int userstack[MOD + 1];    // stack for user verification, starts from 1
+int top = 1;               // sentinel off the end of the stack
+int nonline = 0;           // number of online users
 
 int isonline(int a);
 
+int hash(uint64_t x);      // return an unused hash value in [0, MOD)
+
 int main()
 {
+    setvbuf(stdin, ibuf, _IOFBF, BUF_SZ);
+
     int m;
-    int a, c;
+    uint64_t a;
+    int c;
     char cmd;
     int result = 0;
     scanf("%d%d\n", &m, &m);    // ignore n
@@ -20,20 +29,20 @@ int main()
         scanf("%c", &cmd);
         switch(cmd) {
         case 'I':
-            scanf("%d%d\n", &a, &c);
-            if (isonline(a)) {
-                codestack[users[a]] = c;
+            scanf("%lld%d\n", &a, &c);
+            if (isonline(hash(a))) {
+                codestack[users[hash(a)]] = c;
             } else {
-                users[a] = top;
-                userstack[top] = a;
+                users[hash(a)] = top;
+                userstack[top] = hash(a);
                 codestack[top++] = c;
                 ++nonline;
             }
             break;
         case 'O':
-            scanf("%d\n", &a);
-            if (isonline(a)) {
-                codestack[users[a]] = -1;
+            scanf("%lld\n", &a);
+            if (isonline(hash(a))) {
+                codestack[users[hash(a)]] = -1;
                 --nonline;
             }
             break;
@@ -47,8 +56,10 @@ int main()
             result += nonline;
             break;
         case 'Q':
-            scanf("%d\n", &a);
-            result += isonline(a) ? codestack[users[a]] : -1;
+            scanf("%lld\n", &a);
+            result += isonline(hash(a)) ? codestack[users[hash(a)]] : -1;
+            break;
+        default:
             break;
         }
     }
@@ -60,4 +71,29 @@ int main()
 int isonline(int a)
 {
     return !((users[a] >= top) || (!users[a]) || (codestack[users[a]] == -1) || (userstack[users[a]] != a));
+}
+
+uint64_t hashtable[MOD];    // 0 for vacancy
+
+#define FORWARD 0
+#define BACKWARD 1
+
+int hash(uint64_t x)
+{
+    ++x;    // make sure x is positive
+    int pos = (int)(x % MOD);
+    int direction = FORWARD;
+    uint64_t i = 1;
+    while (hashtable[pos] && hashtable[pos] != x) {
+        if (direction == FORWARD) {
+            pos = (int)(((uint64_t)pos + i * i) % MOD);
+            direction = BACKWARD;
+        } else {
+            pos = (int)(((uint64_t)pos + i * MOD - i * i) % MOD);
+            direction = FORWARD;
+            ++i;
+        }
+    }
+    hashtable[pos] = x;
+    return pos;
 }
